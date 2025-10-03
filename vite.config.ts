4fc379ed7f052048aaf2df487e67782438347caa
Delzinho1 <jailsu49@gmail.com>
2025-10-03 12:52:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/del.eletronics/',  // 👈 nome do repositório aqui
 })
